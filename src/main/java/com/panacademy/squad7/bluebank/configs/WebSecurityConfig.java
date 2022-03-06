fecef303c7196ae26caaa7fb8f4e037b0bb5a93f
William Jesus <williamjesusti@gmail.com>
2022-03-06 14:45:43 -0300

    hotfix: fixed sonar security report

--- src/main/java/com/panacademy/squad7/bluebank/configs/WebSecurityConfig.java
+++ src/main/java/com/panacademy/squad7/bluebank/configs/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.panacademy.squad7.bluebank.configs;
 
 import com.panacademy.squad7.bluebank.configs.security.AuthTokenFilter;
+import com.panacademy.squad7.bluebank.domain.repositories.UsersRepository;
 import com.panacademy.squad7.bluebank.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,8 +24,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UsersService userDetailsService;
+    private final UsersRepository usersRepository;
+
+    public WebSecurityConfig(UsersRepository UsersRepository) {
+        this.usersRepository = UsersRepository;
+    }
 
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
@@ -31,8 +36,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    public void configure(AuthenticationManagerBuilder auth) throws Exception, UsernameNotFoundException {
+        auth.userDetailsService(username -> usersRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username)))
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
